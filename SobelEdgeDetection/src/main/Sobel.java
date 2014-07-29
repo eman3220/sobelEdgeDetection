a52adc3a3069a102c72932cb4e66b24065d6823d
godineemma <godineemma@fyber.ecs.vuw.ac.nz>
2014-07-29 14:31:33 +1200

    Added resources Created main class

---
+++ SobelEdgeDetection/src/main/Sobel.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * input:	image file
+ * output:	image file with edge detection highlights
+ */
+public class Sobel{
+
+	public static void main(String[] args){
+
+
+		new Sobel();
+	}
+
+	public Sobel(){
+
+	}
+
+}
